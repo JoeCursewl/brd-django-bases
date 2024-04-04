40e78a1d0c565535f57373c19aa7e9637548c5b7
Angeldadro <angeldadro@gmail.com>
2024-04-03 23:12:27 -0400

    isa

--- brdbases/settings.py
+++ brdbases/settings.py
@@ -112,11 +112,16 @@ USE_I18N = True
 
 USE_TZ = True
 
+STATIC_URL = "/static/"
+MEDIA_URL = "/public/"
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = "/public/"
+MEDIA_ROOT = BASE_DIR / "media"
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
