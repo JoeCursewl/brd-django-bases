f156b4142262e0514a5517a023538b2cd3e9f0fc
Anthony Cursewl <135174002+JoeCursewl@users.noreply.github.com>
2024-04-09 18:15:54 -0400

    Delete brd-django-bases directory

--- brd-django-bases/notes/forms.py
+++
@@ -1,7 +0,0 @@
-from django.forms import ModelForm
-from .models import Notesreal
-
-class CreateModelForm(ModelForm):
-    class Meta:
-        model = Notesreal
-        fields = ['nota', 'note_content', 'nota_author', 'label_nota']
